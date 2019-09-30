4356aaba4e35c45a03d98bd47c79c5ce5fd9774f
ChiuGypin <crsgypin@gmail.com>
2019-10-01 01:49:17 +0800

    fix

--- app/controllers/linebot/clinics/webhook_controller.rb
+++ app/controllers/linebot/clinics/webhook_controller.rb
@@ -10,6 +10,7 @@ class Linebot::Clinics::WebhookController < Linebot::Clinics::ApplicationControl
 	      config.channel_secret = Rails.application.config_for('api_key')["line"]["channel_secret"]
 	      config.channel_token = Rails.application.config_for('api_key')["line"]["channel_access_token"]
 	    end
+	    @reply_token = params["events"][0]["replyToken"]
 	    linebot.reply_message(@reply_token, reply_message)
 	  end
     render json: {}
