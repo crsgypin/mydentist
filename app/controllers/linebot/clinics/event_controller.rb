1a62f3bdd26e5beab9c66e19f98e0f322dc9be91
ChiuGypin <crsgypin@gmail.com>
2019-10-01 12:00:29 +0800

    style of event page

--- app/controllers/linebot/clinics/event_controller.rb
+++ app/controllers/linebot/clinics/event_controller.rb
@@ -2,6 +2,8 @@ class Linebot::Clinics::EventController < Linebot::Clinics::ApplicationControlle
 
 	def show
 		@line_account = @clinic.line_accounts.find_by!(id: params[:line_account_id])
+		@doctor = @clinic.doctors.find_by(id: params[:doctor_id])
+		@booking_event = @line_account.events.find_by(status: "預約中")
 	end
 
 end
