12c101d52553944ad5085c7920b041976b7bbd07
ChiuGypin <crsgypin@gmail.com>
2019-10-02 10:49:45 +0800

    date time form

--- app/controllers/linebot/clinics/event_controller.rb
+++ app/controllers/linebot/clinics/event_controller.rb
@@ -6,5 +6,9 @@ class Linebot::Clinics::EventController < Linebot::Clinics::ApplicationControlle
 		@booking_event = @line_account.events.find_by(status: "預約中")
 	end
 
+	def update
+		
+	end
+
 end
 
