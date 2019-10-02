6150074d999123cd7671eb0e165a922c11f3f8ee
ChiuGypin <crsgypin@gmail.com>
2019-10-02 11:27:26 +0800

    finished event

--- app/controllers/linebot/clinics/event_controller.rb
+++ app/controllers/linebot/clinics/event_controller.rb
@@ -2,12 +2,24 @@ class Linebot::Clinics::EventController < Linebot::Clinics::ApplicationControlle
 
 	def show
 		@line_account = @clinic.line_accounts.find_by!(id: params[:line_account_id])
-		@doctor = @clinic.doctors.find_by(id: params[:doctor_id])
 		@booking_event = @line_account.events.find_by(status: "預約中")
+
+		@doctor = @clinic.doctors.find_by(id: params[:doctor_id]) || @clinic.doctors.first
 	end
 
 	def update
-		
+		@line_account = @clinic.line_accounts.find_by!(id: params[:line_account_id])
+		@booking_event = @line_account.events.find_by(status: "預約中")
+		if !@booking_event.update(event_params)
+			 return @error_message = @booking_event.errors.full_messages.join("-")
+		end
+	end
+
+
+	private
+
+	def event_params
+		params.require(:event).permit(:doctor_id, :service_id, :date, :hour_minute)
 	end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,19 @@ class Event < ApplicationRecord
 	belongs_to :doctor, optional: true
 	belongs_to :service, optional: true
 	enum status: {"預約中" => 5, "已預約" => 10, "報到" => 15, "預約中取消" => 40, "已預約取消" => 45, "爽約" => 50, "過期" => 55}
+	before_save :check_duration
+
+	def hour_minute=(v)
+		if v.include?(":")
+			v1 = v.split(":")
+			self.hour = v1[0]
+			self.minute = v1[1]
+		end
+	end
+
+	def check_duration
+		self.duration = 15 if self.duration.nil?
+		true
+	end
 
 end
