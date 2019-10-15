a841bfe784f19d63d7e9dc90239857e5feb2d4ca
ChiuGypin <crsgypin@gmail.com>
2019-10-15 20:28:04 +0800

    style

--- app/controllers/clinics/events_controller.rb
+++ app/controllers/clinics/events_controller.rb
@@ -1,12 +1,4 @@
 class ::Clinics::EventsController < ::Clinics::ApplicationController
-  before_action -> {
-    access_config({
-      variable_name: "event",
-      new_resource: proc { @clinic.events.new},
-      find_resource: proc { @clinic.events.find(params[:id])},
-      resource_params: proc { params.require(:event).permit(@event.class.accessable_atts)}
-    })
-  }
 
   def index
     @events = @clinic.events
@@ -14,6 +6,17 @@ class ::Clinics::EventsController < ::Clinics::ApplicationController
 
     @doctors = @clinic.doctors
     @date = Date.parse(params[:date]) rescue Date.today
+  end
+
+  def update
+    @event = @clinic.events.find_by(id: params[:id])
+    @event.update(event_params)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:status)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
 	belongs_to :patient, optional: true
 	belongs_to :doctor, optional: true
 	belongs_to :service, optional: true
-	enum status: {"預約中" => 5, "已預約" => 10, "報到" => 15, "預約中取消" => 40, "已預約取消" => 45, "爽約" => 50, "過期" => 55}
+	enum status: {"預約中" => 5, "已預約" => 10, "報到" => 15, "爽約" => 20, "過期" => 25, "預約中取消" => 40, "已預約取消" => 45}
 	before_save :check_duration
 
 	def hour_minute=(v)
