bc454aa24519ff48aa7651d58eb0d858f5d717fc
ChiuGypin <crsgypin@gmail.com>
2019-10-16 20:48:29 +0800

    updated

--- app/controllers/linebot/clinics/webhook_controller.rb
+++ app/controllers/linebot/clinics/webhook_controller.rb
@@ -15,7 +15,13 @@ class Linebot::Clinics::WebhookController < Linebot::Clinics::ApplicationControl
 	    @line_account.update(reply_token: @reply_token)
 
 	    response = linebot.reply_message(@reply_token, reply_message)
-	    Rails.logger.info "line response, code: #{response.code}, body: #{response.body}"
+	    Rails.logger.info "line response1, code: #{response.code}, body: #{response.body}"
+
+	    response = linebot.reply_message(@reply_token, reply_message)
+	    Rails.logger.info "line response2, code: #{response.code}, body: #{response.body}"
+
+	    response = linebot.reply_message(@reply_token, reply_message)
+	    Rails.logger.info "line response3, code: #{response.code}, body: #{response.body}"
 	  end
     render json: {}
 	end
