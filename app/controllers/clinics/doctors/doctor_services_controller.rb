1104880a0219a909f76dbdb9d260d0fa75393ed9
ChiuGypin <crsgypin@gmail.com>
2019-10-21 19:54:51 +0800

    edit doctor_service

--- app/controllers/clinics/doctors/doctor_services_controller.rb
+++ app/controllers/clinics/doctors/doctor_services_controller.rb
@@ -9,8 +9,34 @@ class ::Clinics::Doctors::DoctorServicesController < ::Clinics::Doctors::Applica
 	end
 
 	def create
-
+		@doctor_service = @doctor.doctor_services.new(doctor_service_params)
+		if !@doctor_service.save
+			@error_messages = @doctor_service.errors.full_messages.join(",")
+			return js_render_error @error_messages
+		end
 	end
 
+	def update
+		@doctor_service = @doctor.doctor_services.find_by!(id: params[:id])
+		if !@doctor_service.update(doctor_service_params)
+			@error_messages = @doctor_service.errors.full_messages.join(",")
+			return js_render_error @error_messages
+		end
+	end
+
+	def destroy
+		@doctor_service = @doctor.doctor_services.find_by!(id: params[:id])
+		if !@doctor_service.destroy
+			@error_messages = @doctor_service.errors.full_messages.join(",")
+			return js_render_error @error_messages
+		end
+	end
+
+
+	private
+
+	def doctor_service_params
+		params.require(:doctor_service).permit(:service_id, :duration)
+	end
 
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,7 @@
 class Service < ApplicationRecord
 	belongs_to :clinic
 	has_many :events
+	has_many :doctor_services, class_name: "Doctor::Service"
+	validates_presence_of :duration
 
 end
