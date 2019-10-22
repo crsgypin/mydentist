1b9de381bf5c9c09efae978a3641aa99de554429
ChiuGypin <crsgypin@gmail.com>
2019-10-22 20:33:34 +0800

    updated

--- app/controllers/linebot/clinics/event_controller.rb
+++ app/controllers/linebot/clinics/event_controller.rb
@@ -4,12 +4,11 @@ class Linebot::Clinics::EventController < Linebot::Clinics::ApplicationControlle
 		@line_account = @clinic.line_accounts.find_by!(id: params[:line_account_id])
 		@booking_event = @line_account.booking_events.last || @line_account.booking_events.new
 
-		@doctor = @clinic.doctors.find_by(id: params[:doctor_id]) || @clinic.doctors.first
+		find_doctor
+		find_service
+		find_date
+		set_event_durations
 
-		@date = Date.parse(params[:date]) rescue Date.today
-		@doctor_durations = @doctor.doctor_durations.where(wday: @date.wday)
-		@doctor_events = @doctor.events.where(date: @date).includes(:events)
-		@doctor_event_durations = @doctor.event_durations.where("events.date = ?", @date)
 		respond_to do |format|
 			format.html
 			format.js
@@ -32,6 +31,69 @@ class Linebot::Clinics::EventController < Linebot::Clinics::ApplicationControlle
 	end
 
 	private
+
+	def find_doctor
+		if params[:doctor_id].present?
+			@doctor = @clinic.doctors.find_by(id: params[:doctor_id])
+		else
+			@doctor = @booking_event.doctor
+		end
+		@doctor = @clinic.doctors.first if @doctor.nil?
+	end
+
+	def find_service
+		if params[:service_id].present?
+			@service = @doctor.services.find_by(id: params[:service_id])
+		else
+			@service = @booking_event.service
+		end
+		@service = @doctor.services.first if @service.nil?
+	end
+
+	def find_date
+		@date = Date.parse(params[:date]) rescue Date.today
+	end
+
+	def set_event_durations
+		@doctor_durations = @doctor.doctor_durations.where(wday: @date.wday)
+		doctor_duration_hours = @doctor_durations.map do |doctor_duration|
+			doctor_duration.hour
+		end
+		@doctor_event_durations = @doctor.event_durations.where("events.date = ?", @date)
+
+		@select_event_durations = [
+			{name: "上午診", hours: @clinic.wday_segment_hours(@date.wday, "早上")},
+			{name: "下午診", hours: @clinic.wday_segment_hours(@date.wday, "下午")},
+			{name: "晚上診", hours: @clinic.wday_segment_hours(@date.wday, "晚上")},
+		].map do |segment|
+			segment_durations = {
+				name: segment[:name],
+				hour_segments: segment[:hours].select do |hour|
+					doctor_duration_hours.include?(hour)
+				end.map do |hour|
+					h = {
+						hour: hour,
+						minute_segments: (60 / Clinic.default_duration).times.map do |index|
+							minute = Clinic.default_duration * index
+							if @date >= Date.today
+								if @doctor_event_durations.find{|d| d.hour == hour && d.minute == minute}
+									enabled = false
+								else
+									enabled = true
+								end
+							else
+								enabled = false
+							end
+							r = {
+								minute: minute,
+								enabled: enabled,
+							}
+						end
+					}
+				end
+			}
+		end
+	end
 
 	def event_params
 		params.require(:event).permit(:doctor_id, :service_id, :date, :hour_minute)
