f6efca6f15f7c0eb19ef7cba54df24c78885b002
ChiuGypin <crsgypin@gmail.com>
2019-10-29 23:58:10 +0800

    system replies

--- app/controllers/clinics/clinic_line/systems_controller.rb
+++ app/controllers/clinics/clinic_line/systems_controller.rb
@@ -4,4 +4,15 @@ class ::Clinics::ClinicLine::SystemsController < ::Clinics::ClinicLine::Applicat
 		@clinic_line_systems = @clinic.clinic_line_systems.includes(:line_template)				
 	end
 
+	def edit
+		@clinic_line_system = @clinic.clinic_line_systems.find(params[:id])
+		@line_template = @clinic_line_system.line_template
+	end
+
+	def update
+		@clinic_line_system = @clinic.clinic_line_systems.find(params[:id])
+		@line_template = @clinic_line_system.line_template
+		update_line_template
+	end
+
 end
