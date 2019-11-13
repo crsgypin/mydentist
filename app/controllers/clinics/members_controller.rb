3c69ab6a4bcb6c79185228a07ad941f7a5029ee1
ChiuGypin <crsgypin@gmail.com>
2019-11-13 12:28:29 +0800

    password

--- app/controllers/clinics/members_controller.rb
+++ app/controllers/clinics/members_controller.rb
@@ -13,6 +13,15 @@ class ::Clinics::MembersController < ::Clinics::ApplicationController
 
   end
 
+  def new
+    @member = @clinic.members.new
+  end
+
+  def edit
+    @member = @clinic.members.find(params[:id])
+
+  end
+
   def create
     @member = @clinic.members.new(member_params)
     if !@member.save
@@ -22,15 +31,27 @@ class ::Clinics::MembersController < ::Clinics::ApplicationController
 
   def update
     @member = @clinic.members.find(params[:id])
-    if @member.update(member_params)
+
+    if params[:target] == "username"
+      #do nothing
+    elsif params[:target] == "password"
+      if !@member.valid_password? params[:ori_password]
+        return js_render_error "錯誤密碼"
+      end      
+    end
+    if !@member.update(member_params)
       return js_render_model_error(@member)      
+    end
+    if params[:target] == "password"
+      sign_out :member
+      sign_in :member, @member
     end
   end
 
   private
 
   def member_params
-    params.require(:member).permit(:level, :username, :email, :password)
+    params.require(:member).permit(:level, :username, :email, :password, :password_confirmation)
   end
 
 end
