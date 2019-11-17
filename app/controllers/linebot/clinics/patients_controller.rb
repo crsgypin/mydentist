25ba13e143e672cc57e62d84c6cae2d284e72acf
ChiuGypin <crsgypin@gmail.com>
2019-11-17 15:18:19 +0800

    updated

--- app/controllers/linebot/clinics/patients_controller.rb
+++ app/controllers/linebot/clinics/patients_controller.rb
@@ -9,14 +9,30 @@ class Linebot::Clinics::PatientsController < Linebot::Clinics::ApplicationContro
 
 	def create
 		@line_account = Line::Account.find_by(line_user_id: params[:line_user_id])
-		@patient = @clinic.patients.find_by(phone: params[:phone])
-		if !@patient.present?
-			return @error_code = 1
-		elsif @patient.line_account.present?
-			return @error_code = 2
+		if params[:action] == "biding"
+			@patient = @clinic.patients.find_by(phone: params[:phone])
+			if !@patient.present?
+				return @error_code = 1
+			elsif @patient.line_account.present?
+				return @error_code = 2
+			end
+			@line_account.update!(patient: @patient)
+		elsif params[:action] == "pateint_form"
+	    ActiveRecord::Base.transaction do 		
+				@patient = @clinic.patients.new(patient_params)
+				if !@patient.save
+					return js_render_model_error @patient
+				end
+				@line_account.update!(patient: @patient)
+			end
 		end
-		@line_account.update!(patient: @patient)
 	end
+
+	private
+
+  def patient_params
+    params.require(:patient).permit(:name, :gender, :person_id, :default_doctor_id, :health_insurance_status, :roc_year, :month, :day, :phone, :phone2, :address, :note)
+  end
 
 end
 
