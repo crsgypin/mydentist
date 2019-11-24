bef27d4e90c6592d75ab0759245ea9681b3a355d
ChiuGypin <crsgypin@gmail.com>
2019-11-24 13:25:34 +0800

    updated

--- app/models/event/notification_schedule.rb
+++ app/models/event/notification_schedule.rb
@@ -1,15 +1,13 @@
 class Event::NotificationSchedule < ApplicationRecord
 	belongs_to :clinic
-	belongs_to :notification_template	
+	belongs_to :notification_template, class_name: "Event::NotificationTemplate", foreign_key: :notification_template_id
 	has_many :notifications
 	enum category: {"回診修改掛號" => 1, "診所休假修改掛號" => 2, "醫生休假修改掛號" => 3, "回診推播" => 4}
 	enum schedule_type: {"立即發送" => 1, "排程發送" => 2}
 	enum status: {"尚未發送" => 0, "發送中" => 1, "已發送" => 2, "取消" => 3}
 	json_format :data
 	before_validation :check_status, on: :update
-	before_create :set_notifications
-	attr_accessor :event_objs
-	attr_accessor :patient_objs
+  accepts_nested_attributes_for :notifications
 
 	private
 
@@ -24,29 +22,29 @@ class Event::NotificationSchedule < ApplicationRecord
 		end
 	end
 
-	def set_notifications
-		if ["回診修改掛號", "診所休假修改掛號", "醫生休假修改掛號"].include?(self.category)
-			event_objs.each do |event_obj| 
-				event = self.clinic.events.find_by(id: event_obj[:id])
-				args = event_obj[:args]
-				self.notifications.build({
-					event: event,
-					line_account: event.patient.line_account,
-					args_json: args
-				})
-			end
-		elsif ["回診推播"].include?(self.category)
-			patient_objs.each do |patient_obj|
-				patient = self.clinic.patients.find_by(id: patient_obj[:id])
-				args = patient_obj[:args]
-				booking_event = patient.booking_events.create!(clinic: @clinic)
-				self.notifications.build({
-					booking_event: booking_event,
-					line_account: patient.line_account,
-					args_json: args
-				})
-			end
-		end
-	end
+	# def set_notifications
+	# 	if ["回診修改掛號", "診所休假修改掛號", "醫生休假修改掛號"].include?(self.category)
+	# 		event_objs.each do |event_obj| 
+	# 			event = self.clinic.events.find_by(id: event_obj[:id])
+	# 			args = event_obj[:args]
+	# 			self.notifications.build({
+	# 				event: event,
+	# 				line_account: event.patient.line_account,
+	# 				args_json: args
+	# 			})
+	# 		end
+	# 	elsif ["回診推播"].include?(self.category)
+	# 		patient_objs.each do |patient_obj|
+	# 			patient = self.clinic.patients.find_by(id: patient_obj[:id])
+	# 			args = patient_obj[:args]
+	# 			booking_event = patient.booking_events.create!(clinic: @clinic)
+	# 			self.notifications.build({
+	# 				booking_event: booking_event,
+	# 				line_account: patient.line_account,
+	# 				args_json: args
+	# 			})
+	# 		end
+	# 	end
+	# end
 
 end
