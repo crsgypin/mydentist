6767e105eaa29dce47b83d707072394e5ae7052f
crsgypin <crsgypin@gmail.com>
2019-12-05 23:29:57 +0800

    add image

--- app/services/linebot_webhook/replies/clinic_line_knowledges_reply.rb
+++ app/services/linebot_webhook/replies/clinic_line_knowledges_reply.rb
@@ -2,10 +2,18 @@ module LinebotWebhook::Replies::ClinicLineKnowledgesReply
 
 	def reply_clinic_line_knowledge
 		@clinic_line_knowledge.line_template.template_messages.map do |m|
-			reply_message({
-				type: "text",
-				text: m.content
-			})
+			if m.category == "text"
+				reply_message({
+					type: "text",
+					text: m.content
+				})
+			elsif m.category == "image"
+				reply_message({
+					type: "image",
+					previewImageUrl: m.file.url,
+					originalContentUrl: m.file.url
+				})
+			end
 		end
 	end
 
