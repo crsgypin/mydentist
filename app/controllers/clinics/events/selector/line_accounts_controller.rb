f0540cf49bfc8ad54fc91002da134377255717a8
crsgypin <crsgypin@gmail.com>
2019-12-06 22:58:23 +0800

    alert lightbox

--- app/controllers/clinics/events/selector/line_accounts_controller.rb
+++ app/controllers/clinics/events/selector/line_accounts_controller.rb
@@ -1,6 +1,11 @@
 class ::Clinics::Events::Selector::LineAccountsController < ::Clinics::ApplicationController
 
 	def new
+		@patient = @clinic.patients.find(params[:patient_id])
+	end
+
+	def create
+		@patient = @clinic.patients.find(params[:patient_id])
 	end
 
 end
